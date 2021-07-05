604bfca276b1b85a570ca4d46c8c6bd6adca8bc4
armansabaa <abdelrahman.elsabaa@gmail.com>
2021-07-05 11:05:40 -0700

    updating both threadids

--- Chat/DataStorage/InMemoryContosoChatData.cs
+++ Chat/DataStorage/InMemoryContosoChatData.cs
@@ -29,8 +29,8 @@ namespace Chat
                 Id = "acs_ve_06_07_2021",
                 ChatSession = new AcsChatSession
                 {
-                    ThreadId = "19:YmjddgUQEvDM6hRDD8eGFTK4fdbqpRD-jVME8zwjed81@thread.v2",
-                    ThreadModeratorId = "8:acs:85c99b9e-f6e1-408c-90d9-e37b6ad0e7c3_0000000a-e65d-db6b-f40f-343a0d00c1d6",
+                    ThreadId = "19:Ytl68hCH36NP5lQFE_EQain8ACl9qOiSdw2dytem4QM1@thread.v2",
+                    ThreadModeratorId = "8:acs:85c99b9e-f6e1-408c-90d9-e37b6ad0e7c3_0000000b-17b9-7405-54b7-a43a0d0017fa",
                 },
                 Rooms = new Dictionary<string, ACSRoom>()
                 {
@@ -42,8 +42,8 @@ namespace Chat
                             Title = "Global All Hands",
                             ChatSession = new AcsChatSession
                             {
-                                ThreadId = "19:s_701MHt__nd5NJT_IZ-zRnSvOTlQ-1pBmGI0xyieHQ1@thread.v2",
-                                ThreadModeratorId = "8:acs:85c99b9e-f6e1-408c-90d9-e37b6ad0e7c3_0000000b-0805-adef-b5bb-a43a0d002005",
+                                ThreadId = "19:gD9iUbus0ZWQjGJ-oATDlX4cu8wH6FeJE7uPe2LFLUQ1@thread.v2",
+                                ThreadModeratorId = "8:acs:85c99b9e-f6e1-408c-90d9-e37b6ad0e7c3_0000000b-17b5-ce49-80f5-8b3a0d00e461",
                             },
                             CallingSessionId = "4fa24250-d478-11eb-a4fa-bb783cfd38e0"
                         }
