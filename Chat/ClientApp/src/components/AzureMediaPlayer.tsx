e41893b6f5b1e4aee4d4ec2b7c9ca3832cb918d2
0rlanand0Wats0n <orwatson@microsoft.com>
2021-07-06 13:00:08 -0700

    turn live transcriptions on

--- Chat/ClientApp/src/components/AzureMediaPlayer.tsx
+++ Chat/ClientApp/src/components/AzureMediaPlayer.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { streamMainStyle } from './styles/Stream.styles';
+import React, { useEffect, } from 'react';
+import "../utils/amp";
 
 export interface AzuremediaplayerProps {
   liveStreamUrl: string;
@@ -13,7 +13,11 @@ export default (props: AzuremediaplayerProps): JSX.Element => {
         controls: true,
         width: '100%',
         height: '100%',
-        poster: ''
+        poster: '',
+        imsc1CaptionsSettings: [{
+          label: 'English',
+          srclang: 'en-us'
+        }]
       };
       let _window: any = window;
       var myPlayer = _window.amp('azuremediaplayer', myOptions);
@@ -45,8 +49,6 @@ export default (props: AzuremediaplayerProps): JSX.Element => {
       poster="poster.jpg"
       data-setup='{"nativeControlsForTouch": false}'
       key={props.liveStreamUrl}
-    >
-      <source src={props.liveStreamUrl} type="application/vnd.ms-sstr+xml" />
-    </video>
+    />
   );
 };
