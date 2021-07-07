0b52e48d097b2f0c1366401dc4b22b9a0923cee8
Bert Ong <823691+beltr0n@users.noreply.github.com>
2021-07-07 10:42:41 -0700

    ui style changes

--- Chat/ClientApp/src/components/OrganizerLandingScreen.tsx
+++ Chat/ClientApp/src/components/OrganizerLandingScreen.tsx
@@ -30,13 +30,12 @@ const iconButtonStyles = {
   },
 };
 
-const [isModalOpen, { setTrue: showModal, setFalse: hideModal }] = useBoolean(false);
-
 export default (props: OrganizerLandingScreenProps): JSX.Element => {
 
   const [title, setTitle] = useState('');
   const [enableChat, setEnableChat] = useState(false);
   const [enableCall, setEnableCall] = useState(false);
+  const [isModalOpen, { setTrue: showModal, setFalse: hideModal }] = useBoolean(false);
 
   const onRoomTopicChange = (event: any) => {
     if (event.target.value) {
@@ -59,27 +58,37 @@ export default (props: OrganizerLandingScreenProps): JSX.Element => {
   const rooms = props.rooms;
   return (
     <div>
-      <PrimaryButton text="Add New Room" onClick={showModal} />
+      <Stack horizontal horizontalAlign="start" verticalAlign="center" tokens={eventHeaderTokens}>
+        <Stack className={upperStackStyle} tokens={upperStackTokens}>
+          <div tabIndex={0} className={headerStyle}>
+            {"Create new rooms as an organizer"}
+          </div>
+          powered by ACS
+        </Stack>
+      </Stack>
+      <Stack horizontal horizontalAlign="end" tokens={{
+        childrenGap: 10,
+        padding: 10
+      }}>
+        <PrimaryButton text="Add New Room" onClick={showModal} />
+        <PrimaryButton
+          onClick={(e) => {
+            props.backToHomeHandler();
+          }}
+            text="Back to Home Page" />
+      </Stack>
       <Modal
         isOpen={isModalOpen}
         onDismiss={hideModal}
         isModeless={true}
       >
         <Stack horizontalAlign="center" verticalAlign="center" tokens={containerTokens}>
-          <Stack horizontal horizontalAlign="center" verticalAlign="center" tokens={eventHeaderTokens}>
-            <Stack className={upperStackStyle} tokens={upperStackTokens}>
-              <div tabIndex={0} className={headerStyle}>
-                {"Create new rooms as an organizer"}
-              </div>
-              powered by ACS
-              <IconButton
-                styles={iconButtonStyles}
-                iconProps={cancelIcon}
-                ariaLabel="Close popup modal"
-                onClick={hideModal}
-              />
-            </Stack>
-          </Stack>
+          <IconButton
+            styles={iconButtonStyles}
+            iconProps={cancelIcon}
+            ariaLabel="Close popup modal"
+            onClick={hideModal}
+          />
           <Stack tokens={outerStackTokens}>
             <TextField label="Room Topic" onChange={onRoomTopicChange} />
             <Checkbox label="Enable Chat" onChange={onEnableChatChange} />
@@ -87,13 +96,12 @@ export default (props: OrganizerLandingScreenProps): JSX.Element => {
             <PrimaryButton
               onClick={async (e) => {
                 await props.createRoom(title, enableChat, enableCall);
+                setTitle("");
+                setEnableCall(false);
+                setEnableChat(false);
+                hideModal();
               }}
               text="Create Room" />
-            <PrimaryButton
-              onClick={(e) => {
-                props.backToHomeHandler();
-              }}
-              text="Back to Home Page" />
           </Stack>
         </Stack>
       </Modal>
