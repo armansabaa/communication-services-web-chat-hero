bb1157ec1dd8957a4b051dcbfe82ccd3ddd58acd
Bert Ong <823691+beltr0n@users.noreply.github.com>
2021-07-07 10:00:45 -0700

    init commit - add back modal

--- Chat/ClientApp/src/components/OrganizerLandingScreen.tsx
+++ Chat/ClientApp/src/components/OrganizerLandingScreen.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Checkbox, CompoundButton, DefaultButton, IStackTokens, PrimaryButton, Stack, TextField } from '@fluentui/react';
+import { Checkbox, CompoundButton, DefaultButton, IconButton, IIconProps, IStackTokens, Modal, PrimaryButton, Stack, TextField } from '@fluentui/react';
 import { containerTokens, descriptionStyle, eventHeaderTokens, headerStyle, upperStackStyle, upperStackTokens } from './styles/HomeScreen.styles';
 import { tilesStackStyles, tilesStackTokens, tileStyle } from './styles/RoomTile.styles';
 import { AcsRoom } from '../core/actions/EventAction';
+import { useBoolean } from '@fluentui/react-hooks';
 
 export interface OrganizerLandingScreenProps {
   backToHomeHandler(): void;
@@ -15,6 +16,22 @@ const outerStackTokens: IStackTokens = {
   childrenGap: 20,
   padding: 10
 };
+
+const cancelIcon: IIconProps = { iconName: 'Cancel' };
+
+const iconButtonStyles = {
+  root: {
+    color: 'gainsboro',
+    marginLeft: 'auto',
+    marginTop: '4px',
+    marginRight: '2px',
+  },
+  rootHovered: {
+    color: 'gray',
+  },
+};
+
+const [isModalOpen, { setTrue: showModal, setFalse: hideModal }] = useBoolean(false);
 
 export default (props: OrganizerLandingScreenProps): JSX.Element => {
 
@@ -43,31 +60,44 @@ export default (props: OrganizerLandingScreenProps): JSX.Element => {
   const rooms = props.rooms;
   return (
     <div>
-      <Stack horizontalAlign="center" verticalAlign="center" tokens={containerTokens}>
-        <Stack horizontal horizontalAlign="center" verticalAlign="center" tokens={eventHeaderTokens}>
-          <Stack className={upperStackStyle} tokens={upperStackTokens}>
-            <div tabIndex={0} className={headerStyle}>
-              {"Create new rooms as an organizer"}
-            </div>
+      <PrimaryButton text="Add New Room" onClick={showModal} />
+      <Modal
+        isOpen={isModalOpen}
+        onDismiss={hideModal}
+        isModeless={true}
+      >
+        <Stack horizontalAlign="center" verticalAlign="center" tokens={containerTokens}>
+          <Stack horizontal horizontalAlign="center" verticalAlign="center" tokens={eventHeaderTokens}>
+            <Stack className={upperStackStyle} tokens={upperStackTokens}>
+              <div tabIndex={0} className={headerStyle}>
+                {"Create new rooms as an organizer"}
+              </div>
               powered by ACS
+              <IconButton
+                styles={iconButtonStyles}
+                iconProps={cancelIcon}
+                ariaLabel="Close popup modal"
+                onClick={hideModal}
+              />
             </Stack>
+          </Stack>
+          <Stack tokens={outerStackTokens}>
+            <TextField label="Room Topic" onChange={onRoomTopicChange} />
+            <Checkbox label="Enable Chat" onChange={onEnableChatChange} />
+            <Checkbox label="Enable Calling" onChange={onEnableCallChange} />
+            <PrimaryButton
+              onClick={async (e) => {
+                await props.createRoom(title, enableChat, enableCall);
+              }}
+              text="Create Room" />
+            <PrimaryButton
+              onClick={(e) => {
+                props.backToHomeHandler();
+              }}
+              text="Back to Home Page" />
+          </Stack>
         </Stack>
-      <Stack tokens={outerStackTokens}>
-          <TextField label="Room Topic" onChange={onRoomTopicChange}/>
-          <Checkbox label="Enable Chat" onChange={onEnableChatChange}/>
-          <Checkbox label="Enable Calling" onChange={onEnableCallChange}/>
-          <PrimaryButton
-            onClick={async (e) => {
-              await props.createRoom(title, enableChat, enableCall);
-            }}
-            text="Create Room" />
-          <PrimaryButton
-            onClick={(e) => {
-              props.backToHomeHandler();
-            }}
-            text="Back to Home Page" />
-        </Stack>
-      </Stack>
+      </Modal>
 
       <Stack horizontal horizontalAlign="space-evenly" styles={tilesStackStyles} tokens={tilesStackTokens}>
         {
