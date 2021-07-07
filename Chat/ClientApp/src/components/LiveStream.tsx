68fa7c288c85c740fae27cdab97eee4d8ecccfa4
Paola Mariana Vicencio Hernandez <pvicencio@microsoft.com>
2021-07-07 17:59:51 -0500

    moving delay after request

--- Chat/ClientApp/src/components/LiveStream.tsx
+++ Chat/ClientApp/src/components/LiveStream.tsx
@@ -33,13 +33,13 @@ export default (props: LiveStreamControlProps): JSX.Element => {
 
   async function getLiveStreamURL() {
     while (!isLiveStreaming) {
-      await delay(2000);
       let result = await startStream(props.roomId);
       if (result && result.liveOutputUrl) {
         startLiveStream(result);
         setIsLiveStreaming(true);
         break;
       }
+      await delay(2000);
     }
   }
 
